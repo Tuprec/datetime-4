e41a0e34f5ad50474ce7e9167497c4ade7629a9c
Thibe Provost <thibe.provost@viso-cor-mariae.be>
2022-04-25 09:16:21 +0200

    Add project files.

---
+++ datetime 4/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace datetime_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime geboorte = new DateTime();
+            DateTime vandaag = new DateTime(2022, 04, 25);
+            Console.WriteLine("geef uw verjaardagsdatum: ");
+            geboorte = Convert.ToDateTime(Console.ReadLine());
+
+            double aantalD = geboorte.Subtract(vandaag).TotalDays;
+
+            Console.WriteLine($"Er zijn nog {aantalD} tot u jarig bent!!!");
+
+            
+
+        }
+    }
+}
